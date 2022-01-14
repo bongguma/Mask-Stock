2f8e2e6eb647b424dfffce7ea3aeaf960df04bc9
kimyejin <7602kyj@e-mirim.hs.kr>
2022-01-14 22:55:42 +0900

    add: http 패키지를 통해 오픈 API 연결 testing

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:developer';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
 
 void main() {
   runApp(const MyApp());
@@ -27,12 +30,14 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  Future fetch() async {
+    var uri = 'https://gist.githubusercontent.com/junsuk5/bb7485d5f70974deee920b8f0cd1e2f0/raw/063f64d9b343120c2cb01a6555cf9b38761b1d94/sample.json?name=%EB%8C%80%EC%A7%80%EC%95%BD%EA%B5%AD';
+
+    var response = await http.get(Uri.parse(uri));
+    log('Response statusCode ${response.statusCode}');
+    log('Response body ${response.body}');
+
   }
 
   @override
@@ -41,7 +46,14 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text('title'),
       ),
-      body: Container(),
+      body: Container(
+        child: TextButton(
+          onPressed: () {
+            fetch();
+          }, child: Text('Click!!'),
+          
+        ),
+      ),
     );
   }
 }
