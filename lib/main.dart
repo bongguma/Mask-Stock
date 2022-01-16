154464ee782676402f884c3822c82f12290785d2
kimyejin <7602kyj@e-mirim.hs.kr>
2022-01-16 15:56:04 +0900

    add: main화면 UI 설계구현

--- lib/main.dart
+++ lib/main.dart
@@ -34,8 +34,12 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
 
   List<Store>? storeList = [];
+  var isLoading = true;
 
   Future fetch() async {
+    setState(() { // loadingBar가 다시 그려져야하기 떄문에 -
+      isLoading = true;
+    });
     var uri = 'https://gist.githubusercontent.com/junsuk5/bb7485d5f70974deee920b8f0cd1e2f0/raw/063f64d9b343120c2cb01a6555cf9b38761b1d94/sample.json?name=%EB%8C%80%EC%A7%80%EC%95%BD%EA%B5%AD';
 
     // get 방식이 비동기로 받아와 await 키워드가 존재하지 않을 시, response값 안에 데이터가 없을 수도 있다.
@@ -44,35 +48,86 @@ class _MyHomePageState extends State<MyHomePage> {
     final jsonResult = jsonDecode(utf8.decode(response.bodyBytes));
     final storeListData = jsonResult['stores'];
 
-    // 동일한 데이터가 쌓일 수 있으므로 한 번 clear 진
-    storeList!.clear();
-    storeListData.foreach((jsonData) {
-      storeList!.add(Store.fromJson(jsonData));
+    setState(() {   // 변경된 상태일 때 화면을 다시 그려야함 -
+      // 동일한 데이터가 쌓일 수 있으므로 한 번 clear 진
+      storeList!.clear();
+      storeListData.foreach((jsonData) {
+        storeList!.add(Store.fromJson(jsonData));
+      });
+
+      isLoading = false;
     });
 
     // log('Response statusCode :: ${response.statusCode}');  // 성공여부
     // log('Response body :: ${response.body}');  // 통신 데이터 출력
-    log('storeList json result :: ${jsonResult['stores']}'); // json형식 storeList
+    // log('storeList json result :: ${jsonResult['stores']}'); // json형식 storeList
 
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    fetch();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('마스크 재고 존재 장소 전체 : ${0}'),
+        title: Text('마스크 재고 존재 장소 전체 : ${storeList!.length}'),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.refresh), onPressed: fetch),
+        ],
       ),
-      body: Container(
-        child: TextButton(
-          onPressed: () async {
-            await fetch();
+      body: !isLoading ? ListView(
+        children: storeList!.map((store) {
+          return ListTile(title: Text(store.name!),
+          subtitle: Text(store.addr!),
+          trailing: Text(store.remainStat! ?? '매진'),);
+        }).toList(),
+      ) : loadingBar(),
+    );
+  }
+}
 
-            // 만약 내가 여기서 storeList 데이터를 출력하고 싶을 때에는 onPressed 비동기가 필수
-            log('storeList :: ${storeList!.toString()}');
-          }, child: Text('Click!!'),
-          
-        ),
+class loadingBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text('데이터를 불러오는 중 ...'),
+        SizedBox(height: 10.0,),
+        CircularProgressIndicator(),
+      ],
+    );
+  }
+}
+
+/*
+* widget 분리 시, function으 분리하는 것보다 class로 분리한다.
+* function은 widget tree가 인식하지 못 한다는 단점과
+* function으로 rebuild 시, function 전체가 reubild된다.
+* class 분리에는 rebuild 시, 재빌드 되야하는 부분만 재빌드된다.
+* */
+class TestTextContainer extends StatelessWidget {
+  const TestTextContainer({Key? key, var storeList}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      child: TextButton(
+        onPressed: () async {
+          // await fetch();
+
+          // 만약 내가 여기서 storeList 데이터를 출력하고 싶을 때에는 onPressed 비동기가 필수
+          // log('storeList :: ${storeList!.toString()}');
+        }, child: Text('Click!!'),
+
       ),
     );
   }
 }
+
